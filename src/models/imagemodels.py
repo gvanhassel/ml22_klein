77d413bc022d122bf1cd1b130eaa317647e2e7f1
Gerard <gvanhassel@gmail.com>
2023-01-10 18:18:30 +0000

    opschonen

--- src/models/imagemodels.py
+++ src/models/imagemodels.py
@@ -4,25 +4,6 @@ from torch import nn
 
 
 @gin.configurable
-class NeuralNetwork(nn.Module):
-    def __init__(self, num_classes: int, units1: int, units2: int) -> None:
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28 * 28, units1),
-            nn.ReLU(),
-            nn.Linear(units1, units2),
-            nn.ReLU(),
-            nn.Linear(units2, num_classes),
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
-
-
-@gin.configurable
 class CNN(nn.Module):
     def __init__(
         self, num_classes: int, kernel_size: int, filter1: int, filter2: int, unit1: int, unit2: int
